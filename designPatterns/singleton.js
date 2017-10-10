e8df75ee975a8a85a615e4c488e08f999c02e469
Jazenye <865388722@qq.com>
2017-10-10 22:40:17 +0800

    singleton update
    增加另一种写法， 注释也是单例模式的核心。
    例子是生成一个遮罩层。

--- designPatterns/singleton.js
+++ designPatterns/singleton.js
@@ -18,3 +18,15 @@ var Person = (function(){
 
 var a = People.createPerson("xxx");  // name: xxx
 var b = People.createPerson("yyy");  // name: yyy
+
+var singleton = function(fn){
+    var result;
+    // 函数作为参数传递, 如果已经创建过，则不会再创建 
+    return function(){
+        return result || (result = fn.apply(this, arguments));
+    };
+}
+
+var createMask = singleton( function(){
+    return document.body.appendChild( document.createElement('div') );
+})
