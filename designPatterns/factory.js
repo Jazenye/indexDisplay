4ef5a8a10041e288f8b0b8709abe0e9cd57e4f0d
Jazenye <865388722@qq.com>
2017-10-10 22:57:46 +0800

    factory explain

--- designPatterns/factory.js
+++ designPatterns/factory.js
@@ -11,3 +11,9 @@ function createPerson(name){
 
 var p = createPerson("jazen");
 var p2 = createPerson("new");
+
+/**
+ * 工厂模式： 由一个方法来决定道理要创建哪个类的实例
+ * 这些实例往往拥有相同的接口。
+ * JS里的构造函数也是一个简单工厂，不过是用new来实现
+ */
