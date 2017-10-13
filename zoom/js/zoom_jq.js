fff924e1054d727f24fb3b11cf18bd6db9f52a8f
Jazenye <865388722@qq.com>
2017-10-13 11:36:56 +0800

    explain added

--- zoom/js/zoom_jq.js
+++ zoom/js/zoom_jq.js
@@ -14,12 +14,16 @@ $(".small").on("mousemove", function(event){
     let localX = event.pageX - $(".small").offset().left + 1;
     let localY = event.pageY - $(".small").offset().top + 1;
 
+    // 将鼠标在图片中的坐标加到HTML中
     $(".local span").eq(0).html(localX);
     $(".local span").eq(1).html(localY);
 
+    // 获取遮罩层mask的左上角坐标
     let maskX = localX - $(".mask").outerWidth(true) / 2;
     let maskY = localY - $(".mask").outerHeight(true) / 2;
  
+    // 判断mask是否越界, 1\2个if判断是否左右溢出
+    // 3\4 if判断 是否上下溢出
     if(maskX < 0) {
         maskX = 0;
     }
@@ -40,12 +44,13 @@ $(".small").on("mousemove", function(event){
 
     // 获取图片原生宽度 jquery 有点问题 
     // $(".big img").outerWidth(true) 获取的是外部宽度而非元素宽度.....!
+    // 获取比例: 大图的宽度 / 缩略图显示的宽度 = 图片移动的距离 / 遮罩层mask移动的距离
     let ratio = parseInt($(".big img").css("width")) / $(".small").outerWidth(true); 
 
     let imgX = ratio * maskX;
     let imgY = ratio * maskY; 
-
-    // 左右移动 imgX差距两个像素, 上下移动 imgY差距两个像素
+ 
+    // 改变图片的margin, 使得局部显示的区域不同
     $(".big img").css({
         marginTop: -imgY + "px",
         marginLeft: -imgX + "px"
