244b7239da10f857e6ed0d372d6feba11cc5e31f
Jazenye <865388722@qq.com>
2017-10-16 11:21:11 +0800

    explain added

--- demo/Promise/promise.js
+++ demo/Promise/promise.js
@@ -1,20 +1,22 @@
 class Promise {
-    constructor (){ 
+    constructor (){
+        // 回调函数队列
         this.callbacks = [];
         this.oncatch = null;
     }
- 
+
+    // resolve： 成功调用后执行
+    // result: 上个函数执行完毕之后传入的参数, 实现数据传递
     resolve(result){
         this.complete("resolve", result);
     }
 
-    // reject
+    // reject: 拒绝后调不再执行后续函数
     reject(result){
         this.complete("reject", result);
     }
 
-   
-
+    // complete 决定调用哪种方法
     complete(type, result){
         if(type === "reject" && this.oncatch){
             this.callbacks = [];
@@ -27,6 +29,8 @@ class Promise {
         }
     }
 
+    // then: 链式调用, 返回自身对象, 把所有函数先加入队列
+    // 参数1: 代表执行成功的方法 
     then(onsuccess, onfail){
         this.callbacks.push({
             resolve: onsuccess,
@@ -35,6 +39,7 @@ class Promise {
         return this;
     }
 
+    // 异常处理, 不在调用后续方法
     catch(onfail){
         this.oncatch = onfail;
         return this;
@@ -62,4 +67,4 @@ function final(result){
     console.log('Finally:', result);
 } 
 
-foo().then(bar ).then(final)
+foo().then(bar).then(final)
